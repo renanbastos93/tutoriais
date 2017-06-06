2e3d8ba46acb9907ea8566a50c21020c27a53c71
RENAN.BASTOS <renanbastos.tec@gmail.com>
2017-06-06 17:34:51 -0300

    [+] example pagination

---
+++ pagination.js
@@ -0,0 +1,32 @@
+
+function listItems(items, pageActual, limitItems){
+	let result = [];
+	let totalPage = Math.ceil( items.length / limitItems );
+	let count = ( pageActual * limitItems ) - limitItems;
+	let delimiter = count + limitItems;
+	
+	if(pageActual <= totalPage){
+		for(let i=count; i<delimiter; i++){
+      if(items[i] != null){
+			  result.push(items[i]);
+      }
+			count++;
+		}
+	}
+
+	return result;
+};
+
+var next = ['Next 1','Next 2','Next 3','Next 4','Next 5'];
+var resultNext = listItems(next, 1, 2);
+var resultNext2 = listItems(next, 2, 2);
+var resultNext3 = listItems(next, 2, 2);
+console.log();
+console.log();
+console.log();
+console.log(" Array Next", next)
+console.log(" Result 1 : ", resultNext)
+console.log(" Result 2 : ", resultNext2)
+console.log(" Result 3 : ", resultNext3)
+console.log();
+console.log();
