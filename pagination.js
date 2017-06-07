a7f4ef5fe225684a3128ea87c303b2eab68761da
renanbastos93 <renanbastos.tec@gmail.com>
2017-06-06 23:22:32 -0300

    [*] improve file

--- pagination.js
+++ pagination.js
@@ -7,9 +7,9 @@ function listItems(items, pageActual, limitItems){
 	
 	if(pageActual <= totalPage){
 		for(let i=count; i<delimiter; i++){
-      if(items[i] != null){
-			  result.push(items[i]);
-      }
+			if(items[i] != null){
+				result.push(items[i]);
+			}
 			count++;
 		}
 	}
@@ -18,15 +18,12 @@ function listItems(items, pageActual, limitItems){
 };
 
 var next = ['Next 1','Next 2','Next 3','Next 4','Next 5'];
+
 var resultNext = listItems(next, 1, 2);
 var resultNext2 = listItems(next, 2, 2);
-var resultNext3 = listItems(next, 2, 2);
-console.log();
-console.log();
-console.log();
+var resultNext3 = listItems(next, 3, 2);
+
 console.log(" Array Next", next)
 console.log(" Result 1 : ", resultNext)
 console.log(" Result 2 : ", resultNext2)
 console.log(" Result 3 : ", resultNext3)
-console.log();
-console.log();
