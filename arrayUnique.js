aede42ac6897f74445051a663cdf9b8e5f1236d3
RENAN.BASTOS <renanbastos.tec@gmail.com>
2018-01-15 14:36:56 -0200

    Update arrayUnique.js

--- arrayUnique.js
+++ arrayUnique.js
@@ -1,1 +1,10 @@
-const arrayUnique = arr => typeof arr[0] === 'object' ? null : [...new Set(arr)];
+function arrayUniqueJSON(arr, param){
+  let uniqueParam = [];
+    for(i = 0; i< arr.length; i++){    
+      if(uniqueParam.indexOf(arr[i][param]) === -1){
+          uniqueParam.push(arr[i][param]);        
+      }        
+  }
+	return uniqueParam;
+}
+const arrayUnique = (arr, param) => typeof arr[0] === 'object' ? arrayUniqueJSON(arr, param) : [...new Set(arr)];
